40eabf3a847e433bf0b44842c7853937668af88b
Vishal Mundlye <vishal.mundlye@citiustech.com>
2017-05-04 20:51:40 +0530

    first commit of functional tests

---
+++ ChefDemo/dist/ChefDemo.Install.1.0/tools/install.ps1
@@ -0,0 +1,27 @@
+param
+(
+    [String]$LogDirectory="C:\temp\ChefDemo.Install.1.0.0",
+    [String]$AdditionalArgs="",
+    [String]$Targets=""
+)
+echo "Running the install..."
+
+$msbuildExe = "$env:windir\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+if(!(Test-Path $msbuildExe))
+{
+    echo "MSBuild not found at $msbuildExe"
+    exit 1
+}
+
+$scriptPath = Split-Path $MyInvocation.MyCommand.Path
+echo "Additional Arguments: $AdditionalArgs"
+# ensure log directory exists
+if(!(Test-Path $LogDirectory))
+{
+    New-Item -Path $LogDirectory -ItemType D
+}
+
+&$msbuildExe $scriptPath\install.proj /target:"$Targets" /property:"$AdditionalArgs" /filelogger /fileloggerparameters:"LogFile=$LogDirectory\install.log"
+
+echo "MSBuild returned exit code $LastExitCode"
+exit $LastExitCode
