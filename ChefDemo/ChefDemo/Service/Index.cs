be763b895333f3dc47838446e3149c474126d49f
Vishal Mundlye <vishal.mundlye@citiustech.com>
2017-06-07 12:05:49 +0530

    fixed the UT, 06-07

--- ChefDemo/ChefDemo/Service/Index.cs
+++ ChefDemo/ChefDemo/Service/Index.cs
@@ -12,7 +12,7 @@ namespace ChefDemo.Service
             return new ChefDemo.Models.Index()
             {
                 FirstName = "Chef",
-                LastName = "Demo1"
+                LastName = "Demo"
             };
         }
     }
