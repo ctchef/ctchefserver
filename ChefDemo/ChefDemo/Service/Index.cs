f384144a24e4c3f86cb54be8672e6b0c5538e8ea
Vishal Mundlye <vishal.mundlye@citiustech.com>
2017-06-07 11:59:25 +0530

    changed the code, 06-07

--- ChefDemo/ChefDemo/Service/Index.cs
+++ ChefDemo/ChefDemo/Service/Index.cs
@@ -12,7 +12,7 @@ namespace ChefDemo.Service
             return new ChefDemo.Models.Index()
             {
                 FirstName = "Chef",
-                LastName = "Demo"
+                LastName = "Demo1"
             };
         }
     }
